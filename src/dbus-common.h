958575cc1c5f3cc9adc50be24804a6955f2c6f8e
Hannu Mallat <hmallat@gmail.com>
2014-11-13 11:17:45 +0200

    sfos: Jolla plugin for D-Bus access check.
    Implement a security plugin for checking D-Bus caller authorization.
    
    The effective gid of the calling process is compared against a
    configurable list to determine whether the process is authorized to
    call restricted D-Bus methods.
    
    By default, in the absence of authorized group configuration, all
    callers are authorized.

--- src/dbus-common.h
+++ src/dbus-common.h
@@ -21,6 +21,10 @@
  *
  */
 
+#define BLUEZ_PRIVILEGED_ACCESS 1
+
+void dict_append_basic(DBusMessageIter *dict, int key_type, const void *key,
+						int type, void *val);
 void dict_append_entry(DBusMessageIter *dict,
 			const char *key, int type, void *val);
 void dict_append_array(DBusMessageIter *dict, const char *key, int type,
