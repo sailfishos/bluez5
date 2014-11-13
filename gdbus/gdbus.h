ccec032dd98681ed6bb24a7137587cf263e8f1d5
Hannu Mallat <hmallat@gmail.com>
2014-11-13 13:03:35 +0200

    sfos: gdbus: use caller's bus name in polkit authorization check
    Use the message sender's bus name instead of bluetooth daemon's own
    bus name in polkit authorization query.
    
    Added the message as a parameter to GDBusSecurityFunction so that the
    sender name (and possibly other message characteristics) can be used
    for authorization.

--- gdbus/gdbus.h
+++ gdbus/gdbus.h
@@ -66,6 +66,7 @@ typedef gboolean (*GDBusPropertyExists)(const GDBusPropertyTable *property,
 typedef guint32 GDBusPendingReply;
 
 typedef void (* GDBusSecurityFunction) (DBusConnection *connection,
+						DBusMessage *message,
 						const char *action,
 						gboolean interaction,
 						GDBusPendingReply pending);
