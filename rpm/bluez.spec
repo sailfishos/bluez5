mer-packages/bluez

[bluez] Add D-Bus interface for debug controlling. Fixes MER#1029

rpm/bluez.spec
@@ -166,7 +166,8 @@ Will enable tracing for BlueZ
     --enable-jolla-dbus-access \
     --enable-gatt \
     --enable-jolla-did \
-    --enable-jolla-wakelock
+    --enable-jolla-wakelock \
+    --enable-jolla-logcontrol
 
 make %{?jobs:-j%jobs}
 

sailfishos/bluez5

Conflicts:
	bluez/Makefile.am
	bluez/acinclude.m4
	rpm/bluez.spec
	bluez/configure.ac

rpm/bluez.spec
@@ -149,7 +149,8 @@ autoreconf --force --install
     --with-systemdsystemunitdir=/lib/systemd/system \
     --with-systemduserunitdir=/usr/lib/systemd/user \
     --enable-jolla-dbus-access \
-    --enable-jolla-did
+    --enable-jolla-did \
+    --enable-jolla-logcontrol
 
 make %{?jobs:-j%jobs}
 