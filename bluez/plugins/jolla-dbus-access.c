mer-packages/bluez

[bluez] Use g_dbus_add_disconnect_watch() for bus name monitoring
It's dangerous to monitor NameOwnerChanged signals directly with
g_dbus_add_signal_watch(), so reimplement D-Bus access check
functionality with g_dbus_add_disconnect_watch()

bluez/plugins/jolla-dbus-access.c
@@ -66,8 +66,7 @@ static GHashTable *gid_hash = NULL;
 static GSList *authorized_gids = NULL;
 static GHashTable *uid_hash = NULL;
 static GSList *authorized_uids = NULL;
-static guint name_changed_watch = 0;
-static guint name_lost_watch = 0;
+static GHashTable *watch_hash = NULL;
 
 static void access_check_free(void *pointer)
 {
@@ -105,46 +104,13 @@ static gboolean uid_is_authorized(uid_t uid)
 	return FALSE;
 }
 
-static gboolean dbus_name_changed(DBusConnection *connection,
-					DBusMessage *message,
-					void *user_data)
+static void busname_exit_callback(DBusConnection *conn, void *user_data)
 {
-	DBusMessageIter iter;
-	char *name;
-
-	if (g_strcmp0(dbus_message_get_signature(message), "sss"))
-		return TRUE;
-
-	dbus_message_iter_init(message, &iter);
-	dbus_message_iter_get_basic(&iter, &name);
-
-	DBG("D-Bus name '%s' changed.", name);
-
-	g_hash_table_remove(gid_hash, name);
-	g_hash_table_remove(uid_hash, name);
-
-	return TRUE;
-}
-
-static gboolean dbus_name_lost(DBusConnection *connection,
-					DBusMessage *message,
-					void *user_data)
-{
-	DBusMessageIter iter;
-	char *name;
-
-	if (g_strcmp0(dbus_message_get_signature(message), "s"))
-		return TRUE;
-
-	dbus_message_iter_init(message, &iter);
-	dbus_message_iter_get_basic(&iter, &name);
-
-	DBG("D-Bus name '%s' lost.", name);
-
-	g_hash_table_remove(gid_hash, name);
-	g_hash_table_remove(uid_hash, name);
-
-	return TRUE;
+	char *busname = user_data;
+	DBG("D-Bus name '%s' gone.", busname);
+	g_hash_table_remove(watch_hash, busname);
+	g_hash_table_remove(gid_hash, busname);
+	g_hash_table_remove(uid_hash, busname);
 }
 
 static void pid_query_result(DBusPendingCall *pend,
@@ -156,6 +122,7 @@ static void pid_query_result(DBusPendingCall *pend,
 	DBusMessageIter iter;
 	dbus_uint32_t pid;
 	struct stat st;
+	guint name_watch;
 
 	DBG("query for busname %s", check->busname);
 
@@ -178,6 +145,14 @@ static void pid_query_result(DBusPendingCall *pend,
 
 	DBG("query done, pid %d has gid %d, uid %d", pid, st.st_gid, st.st_uid);
 
+	name_watch = g_dbus_add_disconnect_watch(check->connection, 
+						check->busname,
+						busname_exit_callback,
+						g_strdup(check->busname),
+						g_free);
+
+	g_hash_table_replace(watch_hash, g_strdup(check->busname),
+				GUINT_TO_POINTER(name_watch));
 	g_hash_table_replace(gid_hash, g_strdup(check->busname),
 				GINT_TO_POINTER(st.st_gid));
 	g_hash_table_replace(uid_hash, g_strdup(check->busname),
@@ -294,19 +269,18 @@ fail:
 	g_dbus_pending_error(connection, pending, DBUS_ERROR_AUTH_FAILED, NULL);
 }
 
+static void unregister_watch(gpointer value)
+{
+	DBusConnection *conn = get_dbus_connection();
+	guint name_watch = GPOINTER_TO_UINT(value);
+	g_dbus_remove_watch(conn, name_watch);
+}
+
 static void jolla_dbus_access_exit(void)
 {
-	DBusConnection *connection = get_dbus_connection();
-
 	g_dbus_unregister_security(security);
-	if (name_lost_watch) {
-		g_dbus_remove_watch(connection, name_lost_watch);
-		name_lost_watch = 0;
-	}
-	if (name_changed_watch) {
-		g_dbus_remove_watch(connection, name_changed_watch);
-		name_changed_watch = 0;
-	}
+	g_hash_table_unref(watch_hash);
+	watch_hash = NULL;
 	g_hash_table_unref(gid_hash);
 	gid_hash = NULL;
 	g_hash_table_unref(uid_hash);
@@ -338,7 +312,6 @@ static GKeyFile *load_config(const char *file)
 
 static int jolla_dbus_access_init(void)
 {
-	DBusConnection *connection = get_dbus_connection();
 	GKeyFile *config;
 
 	config = load_config(CONFIGDIR "/jolla.conf");
@@ -378,35 +351,14 @@ static int jolla_dbus_access_init(void)
 		info("No valid configuration for D-Bus authorized groups"
 			"or users, allowing all");
 
+	watch_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free,
+						unregister_watch);
 	gid_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
 	uid_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
-
-	name_changed_watch = g_dbus_add_signal_watch(connection,
-							"org.freedesktop.DBus",
-							"/org/freedesktop/DBus",
-							"org.freedesktop.DBus",
-							"NameOwnerChanged",
-							dbus_name_changed,
-							NULL,
-							NULL);
-	if (name_changed_watch == 0)
-		goto fail;
-
-	name_lost_watch = g_dbus_add_signal_watch(connection,
-							"org.freedesktop.DBus",
-							"/org/freedesktop/DBus",
-							"org.freedesktop.DBus",
-							"NameLost",
-							dbus_name_lost,
-							NULL,
-							NULL);
-	if (name_lost_watch == 0)
-		goto fail;
 
 	g_dbus_register_security(security);
 	return 0;
 
-fail:
 	jolla_dbus_access_exit();
 	return -1;
 }

sailfishos/bluez5

Conflicts:
	bluez/plugins/jolla-dbus-access.c

bluez/plugins/jolla-dbus-access.c
@@ -63,8 +63,7 @@ static const GDBusSecurityTable security[] = {
 
 static GHashTable *gid_hash = NULL;
 static GSList *authorized_gids = NULL;
-static guint name_changed_watch = 0;
-static guint name_lost_watch = 0;
+static GHashTable *watch_hash = NULL;
 
 static void access_check_free(void *pointer)
 {
@@ -91,50 +90,12 @@ static gboolean gid_is_authorized(gid_t gid)
 	return FALSE;
 }
 
-static gboolean dbus_name_changed(DBusConnection *connection,
-					DBusMessage *message,
-					void *user_data)
+static void busname_exit_callback(DBusConnection *conn, void *user_data)
 {
-	DBusMessageIter iter;
-	char *name;
-
-	if (!gid_hash)
-		return TRUE;
-
-	if (g_strcmp0(dbus_message_get_signature(message), "sss"))
-		return TRUE;
-
-	dbus_message_iter_init(message, &iter);
-	dbus_message_iter_get_basic(&iter, &name);
-
-	DBG("D-Bus name '%s' changed.", name);
-
-	g_hash_table_remove(gid_hash, name);
-
-	return TRUE;
-}
-
-static gboolean dbus_name_lost(DBusConnection *connection,
-					DBusMessage *message,
-					void *user_data)
-{
-	DBusMessageIter iter;
-	char *name;
-
-	if (!gid_hash)
-		return TRUE;
-
-	if (g_strcmp0(dbus_message_get_signature(message), "s"))
-		return TRUE;
-
-	dbus_message_iter_init(message, &iter);
-	dbus_message_iter_get_basic(&iter, &name);
-
-	DBG("D-Bus name '%s' lost.", name);
-
-	g_hash_table_remove(gid_hash, name);
-
-	return TRUE;
+	char *busname = user_data;
+	DBG("D-Bus name '%s' gone.", busname);
+	g_hash_table_remove(watch_hash, busname);
+	g_hash_table_remove(gid_hash, busname);
 }
 
 static void pid_query_result(DBusPendingCall *pend,
@@ -146,6 +107,7 @@ static void pid_query_result(DBusPendingCall *pend,
 	DBusMessageIter iter;
 	dbus_uint32_t pid;
 	struct stat st;
+	guint name_watch;
 
 	DBG("query for busname %s", check->busname);
 
@@ -171,6 +133,14 @@ static void pid_query_result(DBusPendingCall *pend,
 
 	DBG("query done, pid %d has gid %d", pid, st.st_gid);
 
+	name_watch = g_dbus_add_disconnect_watch(check->connection, 
+						check->busname,
+						busname_exit_callback,
+						g_strdup(check->busname),
+						g_free);
+
+	g_hash_table_replace(watch_hash, g_strdup(check->busname),
+				GUINT_TO_POINTER(name_watch));
 	g_hash_table_replace(gid_hash, g_strdup(check->busname),
 				GINT_TO_POINTER(st.st_gid));
 
@@ -266,19 +236,18 @@ fail:
 	g_dbus_pending_error(connection, pending, DBUS_ERROR_AUTH_FAILED, NULL);
 }
 
-static void jolla_dbus_access_exit(void)
+static void unregister_watch(gpointer value)
 {
 	DBusConnection *connection = btd_get_dbus_connection();
+	guint name_watch = GPOINTER_TO_UINT(value);
+	g_dbus_remove_watch(connection, name_watch);
+}
 
+static void jolla_dbus_access_exit(void)
+{
 	g_dbus_unregister_security(security);
-	if (name_lost_watch) {
-		g_dbus_remove_watch(connection, name_lost_watch);
-		name_lost_watch = 0;
-	}
-	if (name_changed_watch) {
-		g_dbus_remove_watch(connection, name_changed_watch);
-		name_changed_watch = 0;
-	}
+	g_hash_table_unref(watch_hash);
+	watch_hash = NULL;
 	g_hash_table_unref(gid_hash);
 	gid_hash = NULL;
 	g_slist_free(authorized_gids);
@@ -306,7 +275,6 @@ static GKeyFile *load_config(const char *file)
 
 static int jolla_dbus_access_init(void)
 {
-	DBusConnection *connection = btd_get_dbus_connection();
 	GKeyFile *config;
 
 	config = load_config(CONFIGDIR "/jolla.conf");
@@ -333,34 +301,13 @@ static int jolla_dbus_access_init(void)
 		info("No valid configuration for D-Bus authorized groups, "
 			"allowing all");
 
+	watch_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free,
+						unregister_watch);
 	gid_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
-
-	name_changed_watch = g_dbus_add_signal_watch(connection,
-							"org.freedesktop.DBus",
-							"/org/freedesktop/DBus",
-							"org.freedesktop.DBus",
-							"NameOwnerChanged",
-							dbus_name_changed,
-							NULL,
-							NULL);
-	if (name_changed_watch == 0)
-		goto fail;
-
-	name_lost_watch = g_dbus_add_signal_watch(connection,
-							"org.freedesktop.DBus",
-							"/org/freedesktop/DBus",
-							"org.freedesktop.DBus",
-							"NameLost",
-							dbus_name_lost,
-							NULL,
-							NULL);
-	if (name_lost_watch == 0)
-		goto fail;
 
 	g_dbus_register_security(security);
 	return 0;
 
-fail:
 	jolla_dbus_access_exit();
 	return -1;
 }