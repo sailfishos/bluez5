f5593967f5cc4665406a84e55b4c149ec2941d55
Franz-Josef Haider <franz.haider@jolla.com>
2017-09-20 09:09:31 -0400

    sfos: bcm43xx: don't flush uart during firmware download. JB#39272

--- tools/hciattach.c
+++ tools/hciattach.c
@@ -50,6 +50,7 @@ struct uart_t {
 	char *bdaddr;
 	int  (*init) (int fd, struct uart_t *u, struct termios *ti);
 	int  (*post) (int fd, struct uart_t *u, struct termios *ti);
+	int do_flush;
 };
 
 #define FLOW_CTL	0x0001
@@ -263,7 +264,7 @@ static int intel(int fd, struct uart_t *u, struct termios *ti)
 
 static int bcm43xx(int fd, struct uart_t *u, struct termios *ti)
 {
-	return bcm43xx_init(fd, u->init_speed, u->speed, ti, u->bdaddr);
+	return bcm43xx_init(fd, u->init_speed, u->speed, ti, u->bdaddr, u->do_flush);
 }
 
 static int read_check(int fd, void *buf, int count)
@@ -1214,7 +1215,7 @@ static void usage(void)
 {
 	printf("hciattach - HCI UART driver initialization utility\n");
 	printf("Usage:\n");
-	printf("\thciattach [-n] [-p] [-b] [-r] [-t timeout] [-s initial_speed]"
+	printf("\thciattach [-n] [-p] [-b] [-r] [-t timeout] [-s initial_speed] [-d]"
 			" <tty> <type | id> [speed] [flow|noflow]"
 			" [sleep|nosleep] [bdaddr]\n");
 	printf("\thciattach -l\n");
@@ -1227,6 +1228,7 @@ int main(int argc, char *argv[])
 	int to = 10;
 	int init_speed = 0;
 	int send_break = 0;
+	int do_flush = 1;
 	pid_t pid;
 	struct sigaction sa;
 	struct pollfd p;
@@ -1237,7 +1239,7 @@ int main(int argc, char *argv[])
 	printpid = 0;
 	raw = 0;
 
-	while ((opt=getopt(argc, argv, "bnpt:s:lr")) != EOF) {
+	while ((opt=getopt(argc, argv, "bnpt:s:lrd")) != EOF) {
 		switch(opt) {
 		case 'b':
 			send_break = 1;
@@ -1268,6 +1270,10 @@ int main(int argc, char *argv[])
 
 		case 'r':
 			raw = 1;
+			break;
+
+		case 'd':
+			do_flush = 0;
 			break;
 
 		default:
@@ -1350,6 +1356,8 @@ int main(int argc, char *argv[])
 	   the hardware's default */
 	if (init_speed)
 		u->init_speed = init_speed;
+
+	u->do_flush = do_flush;
 
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_flags   = SA_NOCLDSTOP;
